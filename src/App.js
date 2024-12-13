5c98c88da20d3f1b5e549874782d3c9a45c565f3
ShtrihKot <sasha.prohorenko.97@mail.ru>
2024-12-13 12:57:27 +0400

    first react app without jsx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { createElement } from 'react';
 
 //декларативный стиль
 export const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-				<p>
-				  {Date().slice(4, 15)}
-			  </p>
-      </header>
-    </div>
+  return createElement(
+    'div',
+    { className: 'App'},
+    createElement(
+      'header',
+      { className : 'App-header'},
+      createElement(
+        'img',
+        { src : logo, className : 'App-logo', alt: 'logo' },
+      ),
+      createElement(
+        'p',
+        null,
+        'Edit ',
+        createElement('code', null, 'src/App.js'),
+        ' and save to reload.',
+      ),
+      createElement(
+        'a',
+        {
+          className: 'App-link',
+          href: 'https://reactjs.org',
+          target: '_blank',
+          rel: 'noopener noreferrer',
+        },
+        'Learn React'
+      ),
+      createElement(
+        'p',
+        null,
+        Date().slice(4, 15),
+      ),
+    ),    
   );
 };
